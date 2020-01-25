772353057ee7f34d7f44747f9c5140cd35012e74
lea <leaprovenzano@gmail.com>
2020-01-25 19:42:08 +0000

    fix up SizeStrategy to just accept elements

--- src/hypothesis_torch/sizes.py
+++ src/hypothesis_torch/sizes.py
@@ -4,8 +4,6 @@ from typing import Any, Sequence, Union
 import torch
 
 from hypothesis import strategies as st
-from hypothesis.strategies._internal.collections import TupleStrategy
-
 from hypothesis.strategies._internal.core import cacheable
 
 from hypothesis.errors import InvalidArgument
@@ -34,28 +32,25 @@ class DimStrategy(st.SearchStrategy[int]):
         return data.draw(self._inner_strat)
 
 
-class SizeStrategy(TupleStrategy):
+class SizeStrategy(st.SearchStrategy[torch.Size]):
 
-    def __init__(self, *dims: Sequence[Union[int, DimStrategy]]):
-        dim_strats = []
-        for d in dims:
-            if not isinstance(d, DimStrategy):
-                if is_valid_dim(d):
-                    dim_strats.append(st.just(d))
-                else:
-                    raise InvalidArgument('dims must be either positive integers or DimStrategy')
-            else:
-                dim_strats.append(d)
-        super().__init__(dim_strats)
+    def __init__(self, elements: st.SearchStrategy):
+        self.element_strategy = elements
 
     def __repr__(self):
         return 'SizeStrategy({", ".join([repr(e) for e in self.element_strategies]))})'
 
-    def do_draw(self, data):
-        return torch.Size(tuple(data.draw(e) for e in self.element_strategies))
+    def do_draw(self, data) -> torch.Size:
+        return torch.Size(data.draw(self.element_strategy))
+
+    def do_validate(self):
+        return self.element_strategy.validate()
+
+    def calc_has_reusable_values(self, recur):
+        return self.element_strategy.calc_has_reusable_values(recur)
 
     def calc_is_empty(self, recur):
-        return any(recur(e) for e in self.element_strategies)
+        return self.element_strategy.calc_is_empty(recur)
 
 
 @cacheable
@@ -100,5 +95,22 @@ def sizes(*dims: Sequence[Union[int, DimStrategy]]) -> SizeStrategy:
         >>> variable_size_strat = sizes(dims(), 5, dims(max_size=10))
         >>> variable_size_strat.example()
         torch.Size([516, 5, 9])
+
+        or use with no arguments to get any old sizes with variable dims...
+        >>> variable_dim_strat = sizes()
+        >>> variable_dim_strat.example()
+        torch.Size([46, 415, 809, 788])
     """
-    return SizeStrategy(*dims)
+    if not dims:
+        return SizeStrategy(st.lists(st.integers(1, 1000), min_size=1, max_size=10))
+    else:
+        dim_strats = []
+        for d in dims:
+            if not isinstance(d, DimStrategy):
+                if is_valid_dim(d):
+                    dim_strats.append(st.just(d))
+                else:
+                    raise InvalidArgument('dims must be either positive integers or DimStrategy')
+            else:
+                dim_strats.append(d)
+    return SizeStrategy(st.tuples(*dim_strats))
